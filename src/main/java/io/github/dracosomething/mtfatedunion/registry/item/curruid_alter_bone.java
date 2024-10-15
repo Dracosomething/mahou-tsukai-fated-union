56296ee316f8816f984d6901b5f4721df0e811a3
Draco_01 <larsfalk08@gmail.com>
2024-10-15 15:05:30 +0200

    v8

---
+++ src/main/java/io/github/dracosomething/mtfatedunion/registry/item/curruid_alter_bone.java
@@ -0,0 +1,9 @@
+package io.github.dracosomething.mtfatedunion.registry.item;
+
+import stepsword.mahoutsukai.item.ItemBase;
+
+public class curruid_bone extends ItemBase {
+    public curruid_bone() {
+        super("curruid_bone");
+    }
+}
